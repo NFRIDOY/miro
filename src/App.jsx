58ecd12c2ad9882413b0688e192bbce7e1144f18
NF RIDOY <nfridoy@gmail.com>
2024-03-02 14:32:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Section from './components/Containers/Section'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Section>
+        j
+      </Section>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Containers/Section.jsx
@@ -0,0 +1,9 @@
+
+// eslint-disable-next-line react/prop-types
+export default function Section({ children }) {
+    return (
+        <div className='max-w-7xl mx-auto'>
+            {children}
+        </div>
+    )
+}
